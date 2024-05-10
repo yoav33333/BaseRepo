834f218bc54d12a8804b0330c093d5420a3c4c81
ADMIN <yoav.2009.goren@gmail.com>
2024-05-10 16:03:56 +0300

    motor use e.g in TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp.java
@@ -1,24 +1,31 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-public class TeleOp extends LinearOpMode {
-    //Variables
+public class TeleOp extends LinearOpMode{
+    int x = 1;
+    double f = 0.5;
+    MotorEx motor;
     @Override
     public void runOpMode() throws InterruptedException {
-        //init
-
-
+        motor = new MotorEx(hardwareMap, "motor");
+        motor.setRunMode(Motor.RunMode.PositionControl);
+        motor.setRunMode(Motor.RunMode.RawPower);
+        motor.setRunMode(Motor.RunMode.VelocityControl);
+        motor.setInverted(true);
+        motor.setInverted(false);
+        motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
         waitForStart();
         while (opModeIsActive()){
-            func(1);
-            //Running Code
+            motor.getVelocity();
+            motor.getCurrentPosition();
+            motor.setTargetPosition(1000);
+            motor.set(1);
+            motor.set(0);
+            motor.set(-1);
         }
-
-
-    }
-    //function
-
-    public void func(double x){
     }
 }
